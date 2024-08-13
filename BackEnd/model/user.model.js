3df7e5d212412f5dfd181f857b28db509e23c41d
MuhammadAreeb30 <almostevil55@gmail.com>
2024-08-13 21:29:50 +0500

    create user model

---
+++ BackEnd/model/user.model.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    validate: (email) => {
+      const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return regex.test(email);
+    },
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  role: {
+    type: String,
+    enum: ["Student", "Recruiter"],
+    required: true,
+  },
+  profile: {
+    bio: {
+      type: String,
+      maxlength: 500,
+    },
+    skills: [{ type: String }],
+    resume: { type: String },
+    resumeOrignialName: { type: String },
+    profilePicture: { type: String, default: "" },
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
