0e2dd6cb5c1618b20e7ad8690d572d12ce3e0713
MuhammadAreeb30 <almostevil55@gmail.com>
2024-08-15 18:35:37 +0500

    company model create

---
+++ BackEnd/model/company.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const companySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    website: {
+      type: String,
+    },
+    location: {
+      type: String,
+    },
+    companyLogo: {
+      type: String,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Company = mongoose.model("Company", companySchema);
+
+export default Company;
