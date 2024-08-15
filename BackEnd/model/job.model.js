89cc5c6405a92334aad2266367a2fa3352e7e232
MuhammadAreeb30 <almostevil55@gmail.com>
2024-08-15 18:35:14 +0500

    job model create

--- BackEnd/model/job.model.js
+++ BackEnd/model/job.model.js
@@ -1,46 +1,52 @@
 import mongoose from "mongoose";
 
-const jobSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  requirements: [
-    {
+const jobSchema = new mongoose.Schema(
+  {
+    title: {
       type: String,
+      required: true,
     },
-  ],
-  jobType: {
-    type: String,
-    required: true,
+    description: {
+      type: String,
+      required: true,
+    },
+    requirements: [
+      {
+        type: String,
+      },
+    ],
+    jobType: {
+      type: String,
+      required: true,
+    },
+    positions: {
+      type: Number,
+      required: true,
+    },
+    salary: {
+      type: Number,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    company: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Company",
+      required: true,
+    },
+    created_by: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    applications: [
+      { type: mongoose.Schema.Types.ObjectId, ref: "Application" },
+    ],
   },
-  positions: {
-    type: Number,
-    required: true,
-  },
-  salary: {
-    type: Number,
-    required: true,
-  },
-  location: {
-    type: String,
-    required: true,
-  },
-  company: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Company",
-    required: true,
-  },
-  created_by: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Company",
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const Job = mongoose.model("Job", jobSchema);
 
