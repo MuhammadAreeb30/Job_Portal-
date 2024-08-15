77f8912a151e7d045973fe10d39a3b95971e6ca4
MuhammadAreeb30 <almostevil55@gmail.com>
2024-08-15 18:53:40 +0500

    models updated

---
+++ BackEnd/model/application.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const applicationSchema = new mongoose.Schema(
+  {
+    job: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Job",
+      required: true,
+    },
+    applicant: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["Pending", "Accepted", "Recjected"],
+      default: "Pending",
+    },
+  },
+  { timestamps: true }
+);
+
+const Application = mongoose.model("Application", applicationSchema);
+
+export default Application;
