35322ea880fef5c7b4bcada88051f41fd2ed3f89
MuhammadAreeb30 <almostevil55@gmail.com>
2024-08-16 20:38:18 +0500

    user router updated

--- BackEnd/Router/user.router.js
+++ BackEnd/Router/user.router.js
@@ -5,12 +5,13 @@ import {
   updateProfile,
   logout,
 } from "../controllers/user.controller.js";
+import isAuthenticated from "../middleware/isAuthenticated.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", register);
 userRouter.post("/login", login);
-userRouter.post("/logout", logout);
-userRouter.patch("/update-profile/:id", updateProfile);
+userRouter.get("/logout", logout);
+userRouter.post("/profile/update", isAuthenticated, updateProfile);
 
 export default userRouter;
