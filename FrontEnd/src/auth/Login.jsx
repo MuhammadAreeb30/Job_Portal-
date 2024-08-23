c11e89198bc61306d9d95029f5e558a8724914fa
MuhammadAreeb30 <almostevil55@gmail.com>
2024-08-23 20:37:25 +0500

    Login Ui Complete

--- FrontEnd/src/auth/Login.jsx
+++ FrontEnd/src/auth/Login.jsx
@@ -17,41 +17,63 @@ const Login = () => {
     <>
       <Navbar />
       <div className="flex justify-center items-center h-[90vh]">
-        <Card className="w-full max-w-sm">
-          <CardHeader>
-            <CardTitle className="text-2xl">Login</CardTitle>
-            <CardDescription>
-              Enter your email below to login to your account.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="grid gap-4">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <Label htmlFor="password">Password</Label>
-              <Input id="password" type="password" required />
-            </div>
-          </CardContent>
-          <CardFooter className="flex flex-col">
-            <Button className="w-full bg-primary">Sign in</Button>
-            <div className="mt-4 text-center text-sm">
-              Don&apos;t have an account?{" "}
-              <Link
-                to="/sign-up"
-                className="underline text-secondary font-medium"
-              >
-                Sign up
-              </Link>
-            </div>
-          </CardFooter>
-        </Card>
+        <form action="">
+          <Card className="w-full max-w-sm">
+            <CardHeader>
+              <CardTitle className="text-2xl">Login</CardTitle>
+              <CardDescription>
+                Enter your email below to login to your account.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="grid gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" type="password" required />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center gap-4">
+                  <div className="flex items-center gap-2">
+                    <Input
+                      name="radio"
+                      type="radio"
+                      className="cursor-pointer w-4"
+                    />
+                    <Label htmlFor="r1">Student</Label>
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <Input
+                      name="radio"
+                      type="radio"
+                      className="cursor-pointer w-4"
+                    />
+                    <Label htmlFor="r2">Recruiter</Label>
+                  </div>
+                </div>
+              </div>
+            </CardContent>
+            <CardFooter className="flex flex-col">
+              <Button className="w-full bg-primary">Sign in</Button>
+              <div className="mt-4 text-center text-sm">
+                Don&apos;t have an account?{" "}
+                <Link
+                  to="/sign-up"
+                  className="underline text-secondary font-medium"
+                >
+                  Sign up
+                </Link>
+              </div>
+            </CardFooter>
+          </Card>
+        </form>
       </div>
     </>
   );
