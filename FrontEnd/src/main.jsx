439ca81aeb47fe6f8f586b06a6de1c782117e26b
MuhammadAreeb30 <almostevil55@gmail.com>
2024-08-24 20:42:02 +0500

    add toastify package

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -3,10 +3,23 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import appRouter from "./router.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-
     <RouterProvider router={appRouter} />
+    <ToastContainer
+      position="top-right"
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="colored"
+    />
   </StrictMode>
 );
